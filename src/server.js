cf8d586dea4bd5d1f97112456388e8d14c9ce487
jotapao <joaopedrocordeiro12.jops@gmail.com>
2025-10-29 20:23:05 -0300

    Criando o middleware para obter o body

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 import http from "node:http";
 
+import { jsonHandler } from "./middlewares/jsonHandler.js";
+
+async function listener(request, response) {}
+
 function listener(request, response) {
   //funcção
 }
