1893aaa230f3949ee042fcb08ebae4b11cb36848
jotapao <joaopedrocordeiro12.jops@gmail.com>
2025-11-04 15:18:29 -0300

    Criando o objeto de ticket

--- src/controllers/tickets/create.js
+++ src/controllers/tickets/create.js
@@ -1,3 +1,17 @@
+import { randomUUID } from "node:crypto";
+
 export function create({ request, response }) {
+  const { equipament, descirption, user_name } = request.body;
+
+  const ticket = {
+    id: randomUUID(),
+    equipament,
+    descirption,
+    user_name,
+    status: "open",
+    created_at: new Date(),
+    updated_at: new Date(),
+  };
+
   return response.end("Criado com sucesso!");
 }
